0f22692b3ec44092f08d91cfd4e8fed966a23f7c
MaxLepan <max.lepan@laposte.net>
2021-09-22 22:44:12 +0200

    Added dynamically executed commands with individual command files

--- botFiles/deploy-commands.js
+++ botFiles/deploy-commands.js
@@ -1,15 +1,20 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
 const { clientId, guildId, TOKEN } = process.env;
 
-const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-    .map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./botFiles/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+
+    const command = require(`${__dirname}/commands/${file}`);
+    commands.push(command.data.toJSON());
+
+}
+
+//-----------------------------------------//
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
 
--- botFiles/index.js
+++ botFiles/index.js
@@ -1,10 +1,21 @@
+const fs = require('fs');
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 require('dotenv').config();
 const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./botFiles/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`${__dirname}/commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -12,18 +23,17 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) {
-        return;
-    }
+    if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply('Server info.');
-    } else if (commandName === 'user') {
-        await interaction.reply('User info.');
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
